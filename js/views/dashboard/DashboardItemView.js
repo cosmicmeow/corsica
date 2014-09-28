984315e8f660d1033376a9e76ca88ef35bd59d0a
vagrant <vagrant@precise64.(none)>
2014-09-28 02:26:55 +0000

    adding dashboard itemview

--- js/views/dashboard/DashboardItemView.js
+++ js/views/dashboard/DashboardItemView.js
@@ -5,18 +5,32 @@ define([
   'text!templates/dashboard/dashboardItemTemplate.html'
 ], function($, _, Backbone, dashboardItemTemplate){
 
+  _.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+  };
+
   var DashboardItemView = Backbone.View.extend({
     el: $("#course_list"),
-
+    template: _.template(
+      '<div class="course row">' +
+        '<div class="col-sm-5">' +
+          '<span class="course_num">{{courseNum}}</span>' +
+          '<span class="course_name">{{desciption}}</span>' +
+        '</div>' +
+        '<div class="col-sm-1"><span class="course_crn">{{crn}}</span></div>' +
+        '<div class="col-sm-3"><span class="course_instructor">{{i_user}}</span></div>' +
+        '<div class="col-sm-3"><span class="course_stat">{{capacity}}</span></div>' +
+      '</div>'
+    ),
     render: function(){
 
       console.log("dashboard -- item");
-      
-      this.$el.append("bOOP");
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
     }
 
   });
 
   return DashboardItemView;
-  
+
 });
