486bd6e6ca808ee442e7ace1d4b128374e45e545
Lookmai Rattana <lrattana@thisisgrow.com>
2014-09-28 19:30:18 +0000

    adding dashbaord view

--- js/views/dashboard/DashboardView.js
+++ js/views/dashboard/DashboardView.js
@@ -5,8 +5,9 @@ define([
   'backbone',
   'text!templates/dashboard/dashboardTemplate.html',
   'views/dashboard/DashboardItemView',
-  'collections/courses/CourseCollection'
-], function($, _, Backbone, DashboardTemplate, DashboardItemView, CourseCollection){
+  'collections/courses/CourseCollection',
+  'models/course/Course'
+], function($, _, Backbone, DashboardTemplate, DashboardItemView, CourseCollection, Course){
 
   var DashboardView = Backbone.View.extend({
     el: $("#page"),
@@ -17,13 +18,26 @@ define([
 
       this.$el.html(DashboardTemplate);
 
-      for (var i = 0; i < 30; i++){
-        dashboardItemView = new DashboardItemView();
-        dashboardItemView.render();
-      }
-      console.log(dashboardItemView);
+      // for (var i = 0; i < 30; i++){
+      //   dashboardItemView = new DashboardItemView();
+      //   dashboardItemView.render();
+      // }
+      //console.log(dashboardItemView);
     },
     initialize: function() {
+      //CourseCollection.fetch();
+
+      var Courses = new CourseCollection();
+      Courses.fetch({
+        success: function(err,data) {
+
+        _.each(data, function(data) {
+          var aCourse = new Course();
+          aCourse.set(data);
+          Courses.add(aCourse);
+        });
+      }});
+      console.log(Courses);
 
     }
   });
