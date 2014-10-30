afa99ae784ec558b07c84a3862450789c7d6ee24
Lookmai Rattana <lrattana@thisisgrow.com>
2014-10-30 16:14:13 +0000

    renmaed directory

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,8 @@ module.exports = function(app, passport) {
 
 	// show the home page (will also have our login links)
 	app.get('/', function(req, res) {
-		res.render('index.ejs');
+		//res.render('login.ejs');
+			res.redirect('/login');
 	});
 
 	// PROFILE SECTION =========================
@@ -37,7 +38,7 @@ module.exports = function(app, passport) {
 
 		// process the login form
 		app.post('/login', passport.authenticate('local-login', {
-			successRedirect : '/profile', // redirect to the secure profile section
+			successRedirect : '/client', // redirect to the secure profile section
 			failureRedirect : '/login', // redirect back to the signup page if there is an error
 			failureFlash : true // allow flash messages
 		}));
@@ -50,7 +51,7 @@ module.exports = function(app, passport) {
 
 		// process the signup form
 		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', // redirect to the secure profile section
+			successRedirect : '/client', // redirect to the secure profile section
 			failureRedirect : '/signup', // redirect back to the signup page if there is an error
 			failureFlash : true // allow flash messages
 		}));
