e558c66c387387a9dabe3683b1f5b644a685c8f0
Lookmai Rattana <lrattana@thisisgrow.com>
2014-11-06 18:16:59 +0000

    fixing gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		sass: {
+			dist: {
+				files: {
+					'public/client/css/styles.css' : 'scss/_main.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			},
+
+		},
+		nodemon: {
+		  dev: {
+		    script: 'server.js'
+		  }
+		}
+	});
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch'],'nodemon');
+};
