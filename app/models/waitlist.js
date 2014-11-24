3ac88543eb6228e650a08c793a3cc1600786b056
Lookmai Rattana <lrattana@thisisgrow.com>
2014-11-24 21:20:42 +0000

    updating what the subscriber model should look like

--- app/models/waitlist.js
+++ app/models/waitlist.js
@@ -1,12 +1,12 @@
 // load the things we need
 var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
-
+ // connect to our database
 // define the schema for our user model
 var waitlistSchema = mongoose.Schema({
   info: {type: mongoose.Schema.Types.Mixed},
   crn: {type: String},
-  subscribers: {type: Array, default: false}, //, unique: true?
+  subscribers: {type: Array, default: Array}, //, unique: true?
   lastModified: { type: Date, default: Date.now },
   notified: { type: Date, default: Date.now }
 });
